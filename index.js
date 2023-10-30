32bed68277aa7796b08f295982b765522b2743bf
Manar_Mamdouh <131297437+Manar504@users.noreply.github.com>
2023-10-30 08:32:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import hello from "./hello world"
+hello()
+
+
+
+import header from "./component/header/header"
+
+import "./style.css"
+
+import cover from"./component/cover/cover"
+
+import bio from "./component/bio/bio"
+
+
+
+
+
+
+
+
+
+
+
+
